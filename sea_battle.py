21f5f1f316217a2161a81ac7508ad3d3d303213a
andWhatShouldISay <zdd-2000@yandex.ru>
2019-04-15 13:07:47 +0300

    поправлен баг
    поправлен баг, при котором можно было тыкать по клеткам после победы

--- sea_battle.py
+++ sea_battle.py
@@ -358,113 +358,114 @@ class Application(Frame):
 	#метод игры компьютера
 
 	def compPlay(self):
-		sleep(1)
-		#если нет точек, в которые попал, но не убил то генерировать случайные точки
-		if len(self.comp_hit)==0:
-			#генерировать случайные точки, пока не будет найдена пара, которой не было в списке выстрелов
-			while 1:
-				i = randrange(10)
-				j = randrange(10)
-				if not("my_"+str(i)+"_"+str(j) in self.comp_shoot):
-					break
-		#если есть одна такая точка
-		elif len(self.comp_hit)==1:
-			#массив точек вокруг
-			points_around = []
-			i = int(self.comp_hit[0].split("_")[1])
-			j = int(self.comp_hit[0].split("_")[2])
-			for ti in range(i-1,i+2):
-				for tj in range(j-1,j+2):
-					if ti>=0 and ti<=9 and tj>=0 and tj<=9  and (ti == i or tj == j) and not(ti == i and tj == j) and not("my_"+str(ti)+"_"+str(tj) in self.comp_shoot):
-						points_around.append([ti,tj])
-			#cлучайная точка из массива
-			select = randrange(len(points_around))
-			i = points_around[select][0]
-			j = points_around[select][1]	
-		else:
-			#если есть больше одной такой точки
-			points_to_strike = []
-			self.comp_hit.sort()
-			#если у таких точек сопадает первая координата
-			if self.comp_hit[0][3]==self.comp_hit[1][3]:
-				#проверяем точку слева от найденных
-				if self.comp_hit[0][5]!='0':
-					arr=self.comp_hit[0].split('_')
-					arr[2]=str(int(arr[2])-1)
-					arr=arr[0]+'_'+arr[1]+'_'+arr[2]
-					if not arr in self.comp_shoot:
-						points_to_strike.append(arr)
-				#справа
-				if self.comp_hit[-1][5]!='9':
-					arr=self.comp_hit[-1].split('_')
-					arr[2]=str(int(arr[2])+1)
-					arr=arr[0]+'_'+arr[1]+'_'+arr[2]
-					if not arr in self.comp_shoot:
-						points_to_strike.append(arr)				
+		if self.checkFinish("user")<10 and self.checkFinish("comp") < 10:
+			sleep(1)
+			#если нет точек, в которые попал, но не убил то генерировать случайные точки
+			if len(self.comp_hit)==0:
+				#генерировать случайные точки, пока не будет найдена пара, которой не было в списке выстрелов
+				while 1:
+					i = randrange(10)
+					j = randrange(10)
+					if not("my_"+str(i)+"_"+str(j) in self.comp_shoot):
+						break
+			#если есть одна такая точка
+			elif len(self.comp_hit)==1:
+				#массив точек вокруг
+				points_around = []
+				i = int(self.comp_hit[0].split("_")[1])
+				j = int(self.comp_hit[0].split("_")[2])
+				for ti in range(i-1,i+2):
+					for tj in range(j-1,j+2):
+						if ti>=0 and ti<=9 and tj>=0 and tj<=9  and (ti == i or tj == j) and not(ti == i and tj == j) and not("my_"+str(ti)+"_"+str(tj) in self.comp_shoot):
+							points_around.append([ti,tj])
+				#cлучайная точка из массива
+				select = randrange(len(points_around))
+				i = points_around[select][0]
+				j = points_around[select][1]	
 			else:
-				#сверху
-				if self.comp_hit[0][3]!='0':
-					arr=self.comp_hit[0].split('_')
-					arr[1]=str(int(arr[1])-1)
-					arr=arr[0]+'_'+arr[1]+'_'+arr[2]
-					if not arr in self.comp_shoot:
-						points_to_strike.append(arr)
-				#снизу				
-				if self.comp_hit[-1][3]!='9':
-					arr=self.comp_hit[-1].split('_')
-					arr[1]=str(int(arr[1])+1)
-					arr=arr[0]+'_'+arr[1]+'_'+arr[2]
-					if not arr in self.comp_shoot:
-						points_to_strike.append(arr)
-			#случайная точка (не больше двух)
-			selected=points_to_strike[randrange(len(points_to_strike))]
-			i=int(selected.split('_')[1])
-			j=int(selected.split('_')[2])
-		xn = j*self.gauge + (j+1)*self.indent + self.offset_x_user
-		yn = i*self.gauge + (i+1)*self.indent + self.offset_y
-		hit_status=0
-		for obj in self.fleet_user:
-			#если координаты точки совпадают с координатой корабля, то вызвать метод выстрела
-			if "my_"+str(i)+"_"+str(j) in obj.coord_map:
-				hit_status=2
-				#изменить статус попадания
-				self.comp_hit.append("my_"+str(i)+"_"+str(j))
-				#мы попали, поэтому надо нарисовать крест
-				self.paintCross(xn,yn,"my_"+str(i)+"_"+str(j))
-				#добавить точку в список выстрелов компьютера
-				self.comp_shoot.append("my_"+str(i)+"_"+str(j))
-				#если метод вернул двойку, значит, корабль убит
-				if obj.shoot("my_"+str(i)+"_"+str(j)) == 2:
-					#изменить статус корабля
-					obj.death = 1
-					#все точки вокруг корабля сделать точками, в которые мы уже стреляли
-					for point in obj.around_map:
-						#нарисовать промахи
-						self.paintMiss(point)
-						#добавить точки вокруг корабля в список выстрелов компьютера
-						self.comp_shoot.append(point)
-					showinfo("","Убил!")
-					self.comp_hit.clear()
+				#если есть больше одной такой точки
+				points_to_strike = []
+				self.comp_hit.sort()
+				#если у таких точек сопадает первая координата
+				if self.comp_hit[0][3]==self.comp_hit[1][3]:
+					#проверяем точку слева от найденных
+					if self.comp_hit[0][5]!='0':
+						arr=self.comp_hit[0].split('_')
+						arr[2]=str(int(arr[2])-1)
+						arr=arr[0]+'_'+arr[1]+'_'+arr[2]
+						if not arr in self.comp_shoot:
+							points_to_strike.append(arr)
+					#справа
+					if self.comp_hit[-1][5]!='9':
+						arr=self.comp_hit[-1].split('_')
+						arr[2]=str(int(arr[2])+1)
+						arr=arr[0]+'_'+arr[1]+'_'+arr[2]
+						if not arr in self.comp_shoot:
+							points_to_strike.append(arr)				
 				else:
-					showinfo("","Попал!")
-				break
-		#если статус попадания остался равным нулю - значит, мы промахнулись, передать управление компьютеру
-		#иначе дать пользователю стрелять
-		if hit_status == 0:
-			#добавить точку в список выстрелов
-			self.comp_shoot.append("my_"+str(i)+"_"+str(j))
-			self.paintMiss("my_"+str(i)+"_"+str(j))
-			showinfo("","Мимо!")
-		else:
-			#проверить выигрыш, если его нет - передать управление компьютеру
-			if self.checkFinish("comp") < 10:
-				self.compPlay()
+					#сверху
+					if self.comp_hit[0][3]!='0':
+						arr=self.comp_hit[0].split('_')
+						arr[1]=str(int(arr[1])-1)
+						arr=arr[0]+'_'+arr[1]+'_'+arr[2]
+						if not arr in self.comp_shoot:
+							points_to_strike.append(arr)
+					#снизу				
+					if self.comp_hit[-1][3]!='9':
+						arr=self.comp_hit[-1].split('_')
+						arr[1]=str(int(arr[1])+1)
+						arr=arr[0]+'_'+arr[1]+'_'+arr[2]
+						if not arr in self.comp_shoot:
+							points_to_strike.append(arr)
+				#случайная точка (не больше двух)
+				selected=points_to_strike[randrange(len(points_to_strike))]
+				i=int(selected.split('_')[1])
+				j=int(selected.split('_')[2])
+			xn = j*self.gauge + (j+1)*self.indent + self.offset_x_user
+			yn = i*self.gauge + (i+1)*self.indent + self.offset_y
+			hit_status=0
+			for obj in self.fleet_user:
+				#если координаты точки совпадают с координатой корабля, то вызвать метод выстрела
+				if "my_"+str(i)+"_"+str(j) in obj.coord_map:
+					hit_status=2
+					#изменить статус попадания
+					self.comp_hit.append("my_"+str(i)+"_"+str(j))
+					#мы попали, поэтому надо нарисовать крест
+					self.paintCross(xn,yn,"my_"+str(i)+"_"+str(j))
+					#добавить точку в список выстрелов компьютера
+					self.comp_shoot.append("my_"+str(i)+"_"+str(j))
+					#если метод вернул двойку, значит, корабль убит
+					if obj.shoot("my_"+str(i)+"_"+str(j)) == 2:
+						#изменить статус корабля
+						obj.death = 1
+						#все точки вокруг корабля сделать точками, в которые мы уже стреляли
+						for point in obj.around_map:
+							#нарисовать промахи
+							self.paintMiss(point)
+							#добавить точки вокруг корабля в список выстрелов компьютера
+							self.comp_shoot.append(point)
+						showinfo("","Убил!")
+						self.comp_hit.clear()
+					else:
+						showinfo("","Попал!")
+					break
+			#если статус попадания остался равным нулю - значит, мы промахнулись, передать управление компьютеру
+			#иначе дать пользователю стрелять
+			if hit_status == 0:
+				#добавить точку в список выстрелов
+				self.comp_shoot.append("my_"+str(i)+"_"+str(j))
+				self.paintMiss("my_"+str(i)+"_"+str(j))
+				showinfo("","Мимо!")
 			else:
-				showinfo("", "Победил компьютер!")
+				#проверить выигрыш, если его нет - передать управление компьютеру
+				if self.checkFinish("comp") < 10:
+					self.compPlay()
+				else:
+					showinfo("", "Победил компьютер!")
 
 	#метод для игры пользователя
 	def userPlay(self,e):
-		if len(self.fleet_user)==10:
+		if len(self.fleet_user)==10 and self.checkFinish("user")<10 and self.checkFinish("comp") < 10:
 			for i in range(10):
 				for j in range(10):
 					xn = j*self.gauge + (j+1)*self.indent + self.offset_x_comp
